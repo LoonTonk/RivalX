b6eaf0dc6f8622bcbe15bbc98e471a702a6fb080
Aidan Liu <aidanyliu@hotmail.com>
2024-08-14 14:38:21 +0900

    Changed notifications on creating  a pattern to be separate with timers, fixed hovering over possible moves to show on colored tiles

--- rivalx.js
+++ rivalx.js
@@ -13,42 +13,6 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 define("bgagame/rivalx", ["require", "exports", "ebg/core/gamegui", "ebg/counter"], function (require, exports, Gamegui) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -544,13 +508,23 @@ define("bgagame/rivalx", ["require", "exports", "ebg/core/gamegui", "ebg/counter
         RivalX.prototype.setupNotifications = function () {
             console.log('notifications subscriptions setup');
             dojo.subscribe('playToken', this, "notif_playToken");
-            this.notifqueue.setSynchronous('playToken', 300);
+            this.notifqueue.setSynchronous('playToken', 500);
             dojo.subscribe('moveWild', this, "notif_moveWild");
-            this.notifqueue.setSynchronous('moveWild', 300);
+            this.notifqueue.setSynchronous('moveWild', 500);
             dojo.subscribe('newScores', this, "notif_newScores");
             this.notifqueue.setSynchronous('newScores', 300);
             dojo.subscribe('scorePattern', this, "notif_scorePattern");
-            this.notifqueue.setSynchronous('scorePattern', 500);
+            this.notifqueue.setSynchronous('scorePattern', 2000);
+            dojo.subscribe('removeTokens', this, "notif_removeTokens");
+            this.notifqueue.setSynchronous('removeTokens', 500);
+            dojo.subscribe('markSelectableTokens', this, "notif_markSelectableTokens");
+            this.notifqueue.setSynchronous('markSelectableTokens', 200);
+            dojo.subscribe('blockadeWin', this, "notif_blockadeWin");
+            this.notifqueue.setSynchronous('blockadeWin', 500);
+            dojo.subscribe('instantWin', this, "notif_instantWin");
+            this.notifqueue.setSynchronous('instantWin', 500);
+            dojo.subscribe('pointsWin', this, "notif_pointsWin");
+            this.notifqueue.setSynchronous('pointsWin', 500);
         };
         RivalX.prototype.notif_playToken = function (notif) {
             this.addTokenOnBoard(notif.args.x, notif.args.y, notif.args.player_id, false);
@@ -583,55 +557,45 @@ define("bgagame/rivalx", ["require", "exports", "ebg/core/gamegui", "ebg/counter
             console.log("add last played to board current player id: " + this.getActivePlayerId());
             this.addLastPlayedToBoard(notif.args.new_x, notif.args.new_y, this.getActivePlayerId());
         };
-        RivalX.prototype.notif_scorePattern = function (notif) {
-            return __awaiter(this, void 0, void 0, function () {
-                var delay, _i, _a, pattern;
-                var _this = this;
-                return __generator(this, function (_b) {
-                    switch (_b.label) {
-                        case 0:
-                            notif.args.selectableTokens.forEach(function (token_pos) {
-                                _this.markSelectableToken(token_pos.x, token_pos.y);
-                            });
-                            delay = function (ms) { return new Promise(function (resolve) { return setTimeout(resolve, ms); }); };
-                            _i = 0, _a = notif.args.patternsToDisplay.patterns;
-                            _b.label = 1;
-                        case 1:
-                            if (!(_i < _a.length)) return [3, 4];
-                            pattern = _a[_i];
-                            this.addPatternOnBoard(pattern, notif.args.patternsToDisplay.x, notif.args.patternsToDisplay.y, notif.args.patternsToDisplay.player_id);
-                            return [4, delay(2000)];
-                        case 2:
-                            _b.sent();
-                            _b.label = 3;
-                        case 3:
-                            _i++;
-                            return [3, 1];
-                        case 4:
-                            ;
-                            notif.args.tokensToRemove.forEach(function (token_pos) {
-                                var token = $("token_".concat(token_pos.x, "_").concat(token_pos.y));
-                                if (token === null) {
-                                    throw new Error("Error: token does not exist in notif_removeTokens");
-                                }
-                                _this.slideToObjectAndDestroy(token, "overall_player_board_".concat(token_pos.player_id));
-                                _this.remainingTokensCounter[parseInt(token_pos.player_id)].incValue(1);
-                            });
-                            notif.args.tokensToRemove.forEach(function (scoretile_pos) {
-                                var scoretile = $("scoretile_".concat(scoretile_pos.x, "_").concat(scoretile_pos.y));
-                                if (scoretile !== null) {
-                                    scoretile.classList.add('toDestroy');
-                                    scoretile.id += '_toDestroy';
-                                }
-                                _this.addTileOnBoard(scoretile_pos.x, scoretile_pos.y, parseInt(scoretile_pos.player_id));
-                            });
-                            document.querySelectorAll('.toDestroy').forEach(function (element) {
-                                dojo.destroy(element);
-                            });
-                            return [2];
-                    }
-                });
+        RivalX.prototype.notif_removeTokens = function (notif) {
+            var _this = this;
+            notif.args.forEach(function (token_pos) {
+                var token = $("token_".concat(token_pos.x, "_").concat(token_pos.y));
+                if (token === null) {
+                    throw new Error("Error: token does not exist in notif_removeTokens");
+                }
+                _this.slideToObjectAndDestroy(token, "overall_player_board_".concat(token_pos.player_id));
+                _this.remainingTokensCounter[parseInt(token_pos.player_id)].incValue(1);
             });
+            notif.args.forEach(function (scoretile_pos) {
+                var scoretile = $("scoretile_".concat(scoretile_pos.x, "_").concat(scoretile_pos.y));
+                if (scoretile !== null) {
+                    scoretile.classList.add('toDestroy');
+                    scoretile.id += '_toDestroy';
+                }
+                _this.addTileOnBoard(scoretile_pos.x, scoretile_pos.y, parseInt(scoretile_pos.player_id));
+            });
+            document.querySelectorAll('.toDestroy').forEach(function (element) {
+                dojo.destroy(element);
+            });
+        };
+        RivalX.prototype.notif_markSelectableTokens = function (notif) {
+            var _this = this;
+            notif.args.forEach(function (token_pos) {
+                _this.markSelectableToken(token_pos.x, token_pos.y);
+            });
+        };
+        RivalX.prototype.notif_scorePattern = function (notif) {
+            this.addPatternOnBoard(notif.args.patternName, notif.args.x, notif.args.y, notif.args.player_id);
+        };
+        RivalX.prototype.notif_pointsWin = function () {
+            console.log("Win by points!");
+        };
+        RivalX.prototype.notif_blockadeWin = function () {
+            console.log("blockade win!");
+        };
+        RivalX.prototype.notif_instantWin = function () {
+            console.log("instant win!");
         };
         RivalX.MAX_WILDS = 5;
         return RivalX;
